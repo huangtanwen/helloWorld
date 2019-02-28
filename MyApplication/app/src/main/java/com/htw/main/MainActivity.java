454fa12a6e793c8015bb7f864e6ba828e81ec321
huangtanwen <527952483@qq.com>
2019-02-28 20:43:55 +0800

    添加textView:

--- MyApplication/app/src/main/java/com/htw/main/MainActivity.java
+++ MyApplication/app/src/main/java/com/htw/main/MainActivity.java
@@ -2,12 +2,20 @@ package com.htw.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mTextView = findViewById(R.id.show_text);
     }
 }
